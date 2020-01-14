1912773cf18a7888b7dc1164108a6b637d9dbf19
a.shtarev <a.shtarev@s7.ru>
2020-01-14 14:12:05 +0300

    user admin page

--- bookclub/bookclub_bot/admin.py
+++ bookclub/bookclub_bot/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
 from .models import Person, BotMessage, InviteIntent, Location, PersonMeeting
 
-admin.site.register(Person)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = (
+        'tg_id', 'tg_username', 'username', 'location',
+        'good_review', 'bad_review', 'not_met_review',
+        'is_blocked'
+    )
+    list_filter = ('is_blocked', )
+    list_per_page = 15
+
+    def good_review(self, person):
+        return (
+            PersonMeeting.objects.filter(
+                from_person=person,
+                rate=PersonMeeting.MeetingRate.GOOD
+            ).count()
+        )
+
+    def bad_review(self, person):
+        return (
+            PersonMeeting.objects.filter(
+                from_person=person,
+                rate=PersonMeeting.MeetingRate.BAD
+            ).count()
+        )
+
+    def not_met_review(self, person):
+        return (
+            PersonMeeting.objects.filter(
+                from_person=person,
+                rate=PersonMeeting.MeetingRate.NOT_MET
+            ).count()
+        )
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Location)
 admin.site.register(BotMessage)
 admin.site.register(InviteIntent)
